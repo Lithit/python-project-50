339aacc4c1d8ffc9e44c2aa466993200e01daf7c
Lithit <rinatha93@mail.ru>
2022-09-16 10:10:38 +0300

    update clii.py

---
+++ gendiff/cli.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def parsing_cli():
+    parser = argparse.ArgumentParser(
+        descpription = 'Compares two configuration\
+        files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
