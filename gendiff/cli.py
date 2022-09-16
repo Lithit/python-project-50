63ea12daa6685717e7b526158b87cb2bb712e3db
ArtShai <rinatha93@mail.ru>
2022-09-16 22:38:01 +0300

    update cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -8,8 +8,12 @@ def parsing_cli():
         description='Compares two configuration\
         files and shows a difference.'
     )
+    # Positional arguments:
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    
+    # Optional arguments:
+    parser.add_argument('-f', '--format', help='set format to output')
     
     args = parser.parse_args()
     
